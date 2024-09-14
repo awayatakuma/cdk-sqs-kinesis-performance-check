958cef53252a4f581a260094a91707b316b13622
awayatakuma <takuma.awaya@gmail.com>
2024-09-14 20:28:34 +0900

    Initial commit

---
+++ test/sqs-kinesis-performance-check.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SqsKinesisPerformanceCheck from '../lib/sqs-kinesis-performance-check-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sqs-kinesis-performance-check-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SqsKinesisPerformanceCheck.SqsKinesisPerformanceCheckStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
